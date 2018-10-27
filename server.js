f4e8fcdd10ed8fe144bb6ab4202d13fcb6da8c5b
MOREASHU <moreashwini65@gmail.com>
2018-10-27 19:39:30 +0530

    server js file added

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use((req,res,next) => {
    
     var now = new Date().toString();
     var log = `${now}: ${req.method} ${req.url}`
+    console.log("inside middelware");
     console.log("log",log);
    next();
 })
